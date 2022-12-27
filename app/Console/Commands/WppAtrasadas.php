56a0e70c0d53d4d78fb08450dc64506c21134ce3
André Jálisson <andrejalisson@gmail.com>
2022-12-27 11:09:08 -0300

    Senha da session

--- app/Console/Commands/WppAtrasadas.php
+++ app/Console/Commands/WppAtrasadas.php
@@ -34,14 +34,14 @@ class WppAtrasadas extends Command
             $cliente = Cliente::where('id', $fatura->cliente_id)->first();
             $wpp = "Olá ".$cliente->name.", tudo bem? 🤩 \n Aqui é da Fortech GPS, pra nós é uma satisfação enorme tê-lo(a) como nosso cliente!  🚀🚀 \n Estou te mandando essa mensagem, para lembrá-lo(a) que seu plano venceu no dia ".date('d/m/Y',  strtotime($fatura->dueDate)).", segue link para pagamento da cobrança";
             Http::withHeaders([
-                'sessionkey' => '31036700'
+                'sessionkey' => 'Aa@31036700.'
             ])->post(env('API_WPP')."/sendText", [
                 'session' => env('SESSION_WPP'),
                 'number' => '5585'.substr($cliente->mobilePhone,3),
                 'text' => $wpp
             ]);
             Http::withHeaders([
-                'sessionkey' => '31036700'
+                'sessionkey' => 'Aa@31036700.'
             ])->post(env('API_WPP')."/sendLink", [
                 'session' => env('SESSION_WPP'),
                 'number' => '5585'.substr($cliente->mobilePhone,3),
