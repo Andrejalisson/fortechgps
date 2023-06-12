8e0844be6cb442ce8cf25209da05afa4a4f4dbc6
André Jálisson <andrejalisson@gmail.com>
2023-06-12 13:37:55 -0300

    Ajuste api

--- app/Helpers/functions.php
+++ app/Helpers/functions.php
@@ -4,22 +4,19 @@ use Illuminate\Support\Facades\Http;
 
 
 function wppTexto($mensagem, $numero){
-    Http::withHeaders([
-        'sessionkey' => 'producao'
-    ])->post(env('API_WPP')."/sendText", [
-        'session' => env('SESSION_WPP'),
-        'number' => '5585'.substr($numero,3),
-        'text' => $mensagem
+    Http::post(env('API_WPP')."/rest/sendMessage/text/?id=".env('TOKEN_WPP'), [
+        'receiver' => '85'.substr($numero,3),
+        'message' => [
+            'text' => $mensagem
+        ]
     ]);
 }
 
 function wppLink($link, $descricao, $numero){
-    Http::withHeaders([
-        'sessionkey' => 'producao'
-    ])->post(env('API_WPP')."/sendLink", [
-        'session' => env('SESSION_WPP'),
-        'number' => '5585'.substr($numero,3),
-        'url' => $link,
-        'text' => $descricao
+    Http::post(env('API_WPP')."/rest/sendMessage/textlink/?id=".env('TOKEN_WPP'), [
+        'receiver' => '85'.substr($numero,3),
+        'message' => [
+            'text' => $descricao." ".$link
+        ]
     ]);
 }
